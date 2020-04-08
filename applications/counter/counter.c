31b99095c7f8d3103b758a30cddb39d83813ac0e
8176135 <elsecaller@8176135.xyz>
2020-04-08 13:58:53 +1200

    Counter update

--- applications/counter/counter.c
+++ applications/counter/counter.c
@@ -4,10 +4,12 @@
 #include "../../libc/string.h"
 
 int counter_main() {
+
     char num[16];
-    for (int i = 0; i < 10000000; ++i) {
-        for (int i2 = 0; i2 < 100000000; ++i2) {
+    for (int i = 0; i < 100000000; ++i) {
+        for (int i2 = 0; i2 < 10000000; i2 += 2) {
             // Waste some clocks
+            i2 -= 1;
         }
         kprint("Stuff: ");
         int_to_ascii(i, num);
